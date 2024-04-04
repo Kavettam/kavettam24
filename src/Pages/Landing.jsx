f27337521691a517f0bab154c831191f66e4866d
sanjay <gamestriker8055@gmail.com>
2024-04-04 21:20:18 +0530

    Update Landing.jsx

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -8,7 +8,7 @@ import CardCarousel from "../Components/CardCarousel";
 
 function Landing() {
   const [showModal, setShowModal] = useState(false);
-  const latestImageUrl = 'https://i.ibb.co/BPTCmDn/day-3.jpg'; // Replace with your latest image URL
+  const latestImageUrl = 'https://i.ibb.co/w6SY31v/dayon1.jpg'; // Replace with your latest image URL
  
   useEffect(() => {
      setShowModal(true);
@@ -54,7 +54,23 @@ function Landing() {
       // title: 'Card 1',
       // description: 'This is the first card.',
     },
+    {
+      image: 'https://i.ibb.co/w6SY31v/dayon1.jpg',
+      // title: 'Card 1',
+      // description: 'This is the first card.',
+    },
+    {
+      image: ' https://i.ibb.co/XY3MvLc/dayon2.jpg',
+      // title: 'Card 1',
+      // description: 'This is the first card.',
+    },
+    {
+      image: 'https://i.ibb.co/D96J3XW/dayon3.jpg',
+      // title: 'Card 1',
+      // description: 'This is the first card.',
+    },
  ];
+
 
   return (
     <div className="Landing">
