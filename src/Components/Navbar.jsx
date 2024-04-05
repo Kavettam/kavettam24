c3dee45908d8748f4ecb5b55ac57e66db5bdc482
sanjay <gamestriker8055@gmail.com>
2024-04-05 11:26:55 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
               </NavLink>
             </li>
             <li className="nav--link">
-              <NavLink onClick={() => toggleChecked(true)} to="/NoPage">
+              <NavLink onClick={() => toggleChecked(true)} to="/Board">
                 Leader Board
               </NavLink>
             </li>
